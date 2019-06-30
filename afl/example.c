ab027b74376fde8c12ab5719f416577c4bd128f4
mgrochowski <gotoco@users.noreply.github.com>
2019-06-30 12:06:31 +0100

    Fromating and comments

--- afl/example.c
+++ afl/example.c
@@ -12,9 +12,11 @@
 int
 main(void)
 {
+	/* AFL by default uses 64kB SHM region for tracing see: 
+	 * https://github.com/mirrorer/afl/blob/master/docs/technical_details.txt#L37 */
+	uint64_t size = (1 << 16);
         char *cover, n;
 	int i = 0;
-        uint64_t size = (1 << 16);
         int fd;
         int mode;
 
@@ -24,17 +26,20 @@ main(void)
         if (ioctl(fd, KCOV_IOC_SETBUFSIZE, &size) == -1)
                 err(1, "ioctl: KCOV_IOC_SETBUFSIZE");
         cover = mmap(NULL, size,
-            PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+                     PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
         if (cover == MAP_FAILED)
                 err(1, "mmap");
         mode = KCOV_MODE_TRACE_PC;
         if (ioctl(fd, KCOV_IOC_ENABLE, &mode) == -1)
                 err(1, "ioctl: KCOV_IOC_ENABLE");
-        read(-1, NULL, 0); /* syscall paths to be traced */
-	printf("#: PRINTF TEST! \n");
+	/* Start of code to be traced */
+        read(-1, NULL, 0); 
+	printf("#: PRINTF TRACE!\n");
+	/* End of traced code */
 	
         if (ioctl(fd, KCOV_IOC_DISABLE) == -1)
                 err(1, "ioctl: KCOV_IOC_DISABLE");
+	/* Print SHM region after the tracing */
 	for (i = 0; i < size; ++i) {
 		if (i % 32 == 31) printf("\n");
 	
